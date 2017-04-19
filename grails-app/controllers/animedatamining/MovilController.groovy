83b15899e80e970084ab4035d273bac675ac78ee
mrmomo <un.own.cry@gmail.com>
2017-04-18 22:14:42 -0400

    agregando el chequeo imagenes

--- grails-app/controllers/animedatamining/MovilController.groovy
+++ grails-app/controllers/animedatamining/MovilController.groovy
@@ -9,16 +9,33 @@ import java.awt.image.BufferedImage
 class MovilController {
 
     def imagenService
+    def ClassifyImageService
+
     def index() {
         render "Bienvenidos al API REST"
     }
 
-    def procesarImagen(String imagen ){
-        println "SUPERLOG:"+imagen
+    def dirPath() {
+        //ejmplo tiene que ser la ruta completa
+        String testImg = "/home/mrmomo/gitStuff/dataMiningStuff/AnimeDataMining/Screenshot from 2017-01-16 18-17-25.png"
+        //ruta completa a donde esta la capeta final que tiene lo necesario
+        String finalDir = servletContext.getRealPath("/final")
+        Boolean imageType = ClassifyImageService.isCartoon(finalDir,testImg)
+        if (imageType == null) {
+            render "no image"
+        } else if (imageType) {
+            render "cartoon"
+        } else {
+            render "real"
+        }
+    }
+
+    def procesarImagen(String imagen) {
+        println "SUPERLOG:" + imagen
         RetornoImagen retorno = new RetornoImagen()
         try {
             retorno = imagenService.procesarImagen(imagen)
-        }catch (MiExcepcion ex){
+        } catch (MiExcepcion ex) {
             ex.printStackTrace()
             retorno = new RetornoImagen()
             retorno.error = true
